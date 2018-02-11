00a3e610c593c336a5784765f32b652c040cd1fa
Emma Loeb <emma.loeb@wework.com>
2018-02-11 21:18:39 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-    puts "#{board[0]}"|"{board[1]}   "|"   " 
+    puts "#{board[0]}"|" #{board[1]}   "|"   " 
     puts "-----------"
     puts "   "|"   "|"   " 
     puts "-----------"
